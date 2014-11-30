f4687e29b7068ee3d8a40f370cfdf027f9cd783f
Tomer Levi <Tomer.Levi@Klarna.com>
2014-11-30 13:58:53 +0200

    Semantic changes in klarna registration view-controller

--- inapp/KIARegistrationViewController.m
+++ inapp/KIARegistrationViewController.m
@@ -22,18 +22,47 @@ id<KIARegistrationViewControllerDelegate> delegate;
 - (void)viewDidLoad {
   [super viewDidLoad];
   
+  if(self.navigationController)
+    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Cancel" style:UIBarButtonItemStylePlain target:self action:@selector(cancelButtonPressed)];
   
-  self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Cancel" style:UIBarButtonItemStylePlain target:self action:@selector(cancelButtonPressed)];
-  
+  [self AddWebView];
+
+  [self AddSpinner];
+}
+
+- (void)AddWebView {
   UIWebView *webview = [[UIWebView alloc] initWithFrame:self.view.frame];
   webview.delegate = self;
   NSURLRequest *request = [NSURLRequest requestWithURL:[KIAUrl registrationUrl] cachePolicy:NSURLRequestReloadIgnoringCacheData timeoutInterval:60.0f];
   [webview loadRequest:request];
   [self.view addSubview:webview];
-  webview.backgroundColor = [UIColor whiteColor];
-
-  [self AddSpinner];
 }
+
+- (void)AddSpinner {
+  _spinnerView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, 80, 80)];
+  _spinnerView.center = self.view.center;
+  _spinnerView.backgroundColor = [UIColor colorWithWhite:0. alpha:0.6];
+  _spinnerView.layer.cornerRadius = 5;
+  
+  UIActivityIndicatorView *activityView= [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhiteLarge];
+  activityView.center = CGPointMake(_spinnerView.frame.size.width / 2.0, 35);
+  [activityView startAnimating];
+  [_spinnerView addSubview:activityView];
+  
+  UILabel* lblLoading = [[UILabel alloc]initWithFrame:CGRectMake(0, 48, 80, 30)];
+  lblLoading.text = @"Loading...";
+  lblLoading.textColor = [UIColor whiteColor];
+  lblLoading.font = [UIFont fontWithName:lblLoading.font.fontName size:15];
+  lblLoading.textAlignment = NSTextAlignmentCenter;
+  [_spinnerView addSubview:lblLoading];
+  
+  [self.view addSubview:_spinnerView];
+}
+
+- (void)RemoveSpinnerIfExists {
+  [_spinnerView removeFromSuperview];
+}
+
 
 - (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error {
   [self RemoveSpinnerIfExists];
@@ -56,31 +85,5 @@ id<KIARegistrationViewControllerDelegate> delegate;
 - (void)webViewDidFinishLoad:(UIWebView *)webView {
   [self RemoveSpinnerIfExists];
 }
-
-- (void) AddSpinner {
-  _spinnerView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, 80, 80)];
-  _spinnerView.center = self.view.center;
-  _spinnerView.backgroundColor = [UIColor colorWithWhite:0. alpha:0.6];
-  _spinnerView.layer.cornerRadius = 5;
-  
-  UIActivityIndicatorView *activityView= [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhiteLarge];
-  activityView.center = CGPointMake(_spinnerView.frame.size.width / 2.0, 35);
-  [activityView startAnimating];
-  [_spinnerView addSubview:activityView];
-  
-  UILabel* lblLoading = [[UILabel alloc]initWithFrame:CGRectMake(0, 48, 80, 30)];
-  lblLoading.text = @"Loading...";
-  lblLoading.textColor = [UIColor whiteColor];
-  lblLoading.font = [UIFont fontWithName:lblLoading.font.fontName size:15];
-  lblLoading.textAlignment = NSTextAlignmentCenter;
-  [_spinnerView addSubview:lblLoading];
-  
-  [self.view addSubview:_spinnerView];
-}
-
-- (void) RemoveSpinnerIfExists {
-  [_spinnerView removeFromSuperview];
-}
-
 
 @end
