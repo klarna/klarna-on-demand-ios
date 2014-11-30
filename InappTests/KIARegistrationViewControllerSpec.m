f5686e3640d8527efacb04a89ab0ec2528c81c29
Tomer Levi <Tomer.Levi@Klarna.com>
2014-11-30 14:12:30 +0200

    added klarnaRegistrationCancelled spec

--- InappTests/KIARegistrationViewControllerSpec.m
+++ InappTests/KIARegistrationViewControllerSpec.m
@@ -1,19 +1,31 @@
 #import "KIARegistrationViewController.h"
-/*
-@interface KIARegistrationViewController (Testing)
 
-- (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error;
+@interface KIARegistrationViewController (Test)
+- (void)cancelButtonPressed;
 @end
-*/
+
 SPEC_BEGIN(KIARegistrationViewControllerSpec)
 
 describe(@"KIARegistrationViewControllerSpec", ^{
-  it(@"should ...",^{
+  
+  it(@"should conform to UIWebViewDelegate protocol", ^ {
+    [[[KIARegistrationViewController mock] should] conformToProtocol:@protocol(UIWebViewDelegate)];
+  });
+  
+  it(@"should call .klarnaRegistrationCancelled when cancel button was pressed",^{
+    id kiaRegistrationDelegate = [KWMock mockForProtocol:@protocol(KIARegistrationViewControllerDelegate)];
+    KIARegistrationViewController *kiaRegistrationController = [[KIARegistrationViewController alloc] initWithDelegate:kiaRegistrationDelegate];
+    [[[kiaRegistrationDelegate shouldEventually] receive] klarnaRegistrationCancelled:kiaRegistrationController];
+    [kiaRegistrationController cancelButtonPressed];
+  });
+  
+  it(@"should call .klarnaRegistrationFailed when web view failed to load",^{
     id kiaRegistrationDelegate = [KWMock mockForProtocol:@protocol(KIARegistrationViewControllerDelegate)];
     KIARegistrationViewController *kiaRegistrationController = [[KIARegistrationViewController alloc] initWithDelegate:kiaRegistrationDelegate];
     [[[kiaRegistrationDelegate shouldEventually] receive] klarnaRegistrationFailed:kiaRegistrationController];
     [kiaRegistrationController webView:nil didFailLoadWithError:[NSError errorWithDomain:@"Domain" code:1234 userInfo:nil]];
   });
+  
 });
 
 SPEC_END
