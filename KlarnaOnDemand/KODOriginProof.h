7f9ccd8b0fdc4de2fa6fe0c1f6f87425029e9b6d
Guy Rozen <holyeggplant@gmail.com>
2015-01-20 16:20:57 +0200

    Updated method descriptions

--- KlarnaOnDemand/KODOriginProof.h
+++ KlarnaOnDemand/KODOriginProof.h
@@ -12,14 +12,14 @@
  *  @param currency  The currency used in this purchase ("SEK", "EUR", or other <a href="http://en.wikipedia.org/wiki/ISO_4217">ISO 4217 codes</a>)
  *  @param userToken Token identifying the user making the purchase
  *
- *  @return An origin proof that needs to be sent along with the purchase for it to succeed.
+ *  @return An origin proof that matches the purchase details and the device that issued the purchase.
  */
 - (id)initWithAmount:(int)amount currency:(NSString *)currency userToken:(NSString *)userToken;
 
 /**
- *  Generates an origin proof NString that matches the details of the purchase it will be used to verify.
+ *  Returns a textual representation of the origin proof that needs to be sent along with the purchase for it to succeed.
  *
- *  @return An origin proof NString that needs to be sent along with the purchase for it to succeed.
+ *  @return A textual representation of the origin proof that needs to be sent along with the purchase for it to succeed.
  */
 - (NSString *)description;
 
