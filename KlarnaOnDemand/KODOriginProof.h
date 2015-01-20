bde10848644a6cae0e5777c37a7da9ca9e9d933c
Tomer Levi <Tomer.Levi@Klarna.com>
2015-01-20 17:03:48 +0200

    modify KODOriginProof's documentation

--- KlarnaOnDemand/KODOriginProof.h
+++ KlarnaOnDemand/KODOriginProof.h
@@ -6,7 +6,7 @@
 @interface KODOriginProof : NSObject
 
 /**
- *  Generates an origin proof that matches the details of the purchase it will be used to verify.
+ *  Generates an origin proof that can be used to verify the details of the purchase.
  *
  *  @param amount    The purchase's total amount in "cents"
  *  @param currency  The currency used in this purchase ("SEK", "EUR", or other <a href="http://en.wikipedia.org/wiki/ISO_4217">ISO 4217 codes</a>)
