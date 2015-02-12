643bfc162c322174015710d27c11b0cc7aed9c32
Guy Rozen <holyeggplant@gmail.com>
2015-02-12 15:45:17 +0200

    An entire button is being clicked

---
+++ features/step_definitions/typical_steps.rb
@@ -0,0 +1,11 @@
+When(/^I launch the sample application and press '(.+)'$/) do |button_text|
+  button(button_text).click
+end
+
+When(/^I complete the registration process$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see a QR code for the ticket I bought$/) do
+  pending # express the regexp above with the code you wish you had
+end
