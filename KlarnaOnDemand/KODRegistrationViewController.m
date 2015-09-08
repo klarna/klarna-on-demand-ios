d65a4e2cc13f421224b0f24166252b3e7395f703
Tomer Levi <Tomer.Levi@Klarna.com>
2015-09-08 22:34:50 +0300

    add one second delay before notifying for user error

--- KlarnaOnDemand/KODRegistrationViewController.m
+++ KlarnaOnDemand/KODRegistrationViewController.m
@@ -63,7 +63,9 @@
 
 - (void)handleUserErrorEvent {
   if ([self.delegate respondsToSelector:@selector(klarnaRegistrationFailed:)]) {
-    [self.delegate klarnaRegistrationFailed:self];
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+      [self.delegate klarnaRegistrationFailed:self];
+    });
   }
 }
 
